ad37930daced270a67e32e3d8ed50a06abbe5feb
gabrielrstan <51632457+gabrielrstan@users.noreply.github.com>
2024-04-21 19:09:08 -0300

    Termino da seção 17

--- Secao17/structural/flyweight.py
+++ Secao17/structural/flyweight.py
@@ -31,3 +31,84 @@ algo; inerente.
 Extrínseco - que não pertence à essência de algo;
 que é exterior.
 """
+from __future__ import annotations
+
+from typing import Dict, List
+
+
+class Client:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self._addresses: List = []
+        self.address_number: str
+        self.address_details: str
+
+    def add_address(self, address: Address) -> None:
+        self._addresses.append(address)
+
+    def list_addresses(self) -> None:
+        for address in self._addresses:
+            address.show_address(self.address_number, self.address_details)
+
+
+class Address:
+    def __init__(self, street: str, neighborhood: str, zip_code: str) -> None:
+        self._street = street
+        self._neighborhood = neighborhood
+        self._zip_code = zip_code
+
+    def show_address(self, address_number: str, address_details: str) -> None:
+        print(
+            self._street, address_number, self._neighborhood, address_details,
+            self._zip_code
+        )
+
+
+class AddressFactory:
+    _addresses: Dict = {}
+
+    def _get_key(self, **kwargs) -> str:
+        return ''.join(kwargs.values())
+
+    def get_address(self, **kwargs) -> Address:
+        key = self._get_key(**kwargs)
+
+        try:
+            address_flyweight = self._addresses[key]
+            print('Usando objeto já criado')
+        except KeyError:
+            address_flyweight = Address(**kwargs)
+            self._addresses[key] = address_flyweight
+            print('Criando novo objeto')
+
+        return address_flyweight
+
+
+if __name__ == '__main__':
+    address_factory = AddressFactory()
+
+    a1 = address_factory.get_address(
+        street='Rua 1',
+        neighborhood='Centro',
+        zip_code='12345-678'
+    )
+
+    a2 = address_factory.get_address(
+        street='Rua 1',
+        neighborhood='Centro',
+        zip_code='12345-678'
+    )
+
+    gabriel = Client('Gabriel')
+    gabriel.address_number = '12345'
+    gabriel.address_details = 'Casa'
+    gabriel.add_address(a1)
+    gabriel.list_addresses()
+
+    joana = Client('Joana')
+    joana.address_number = '125B'
+    joana.address_details = 'AP 326'
+    joana.add_address(a2)
+    joana.list_addresses()
+
+    print(a1 == a2)
